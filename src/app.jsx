443438e321945c3aeded67ffcba0712cc30f139a
psagrammer <parsasagharzadehbit@gmail.com>
2022-08-21 13:35:03 +0300

    project  created

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import contextUse from "./context/context";
+const App = () => {
+    return (<>
+    <contextUse.Provider  value={{}} >
+
+
+
+
+    </contextUse.Provider>
+    
+    </>  );
+}
+ 
+export default App;
