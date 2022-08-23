1cc8bf0aa491a6daa5d1c6eab6b9cccdda544d52
psagrammer <parsasagharzadehbit@gmail.com>
2022-08-23 18:00:05 +0300

    betpage apgraded2

--- src/component/betpage/betpage.jsx
+++ src/component/betpage/betpage.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import Dropdown from 'react-bootstrap/Dropdown';
 import contextUse from "../../context/context";
 import queryString from  "query-string"
 import './betpage.css'
@@ -12,7 +13,34 @@ const BetPage = (props) => {
     console.log(game)
 
     return ( <>
-    <h1 className="text-dark" >gfhfghfghfg</h1>
+    <section className="betpage" >
+<div className="bets" >
+    <h1 className="bet" ><em><b>BeTs</b></em></h1>
+</div>
+<div className="main" >
+<h1 className="title" >{sportId.page}</h1><br />
+<h1 className="text-center text-warning my-3 " >
+{sportId.games[game].nametournaments}
+</h1 >
+<h2 className="text-center text-warning my-3 ">
+{sportId.games[game].game}
+
+</h2>
+<div className="d-flex justify-content-around" >
+{
+sportId.games[game].teams.map((e ,i)=>{
+  return  <>
+  <div key={i} >
+  <h1 className="text-center text-warning my-3 " key={i} >{e}</h1><br />
+  </div>
+
+  </>
+})    
+}
+</div>
+
+</div>
+    </section>
     
     </> );
 }
