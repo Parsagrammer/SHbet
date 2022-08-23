f8a6838e06e2dab2993b94a4b7b0eec1f5f4a2f5
psagrammer <parsasagharzadehbit@gmail.com>
2022-08-23 12:40:40 +0300

    betpage upgraded

--- src/component/betpage/betpage.jsx
+++ src/component/betpage/betpage.jsx
@@ -6,6 +6,11 @@ import './betpage.css'
 
 const BetPage = (props) => {
     const contextuse = useContext(contextUse)
+    let sportId = contextuse.gamePage[Number (queryString.parse(window.location.search).sportId)]
+    let game = Number (queryString.parse(window.location.search).game)
+    console.log(sportId)
+    console.log(game)
+
     return ( <>
     <h1 className="text-dark" >gfhfghfghfg</h1>
     
